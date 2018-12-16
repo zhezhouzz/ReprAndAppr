e11ec64f0ec3419b231dc3c43213f8211177deea
zhouzhezz <zorichzorich@gmail.com>
2018-12-16 12:03:07 -0500

    Use record in repr list

--- repr.ml
+++ repr.ml
@@ -1,12 +1,11 @@
 module type Reprlist = sig
-
   type sfunction
 
   type vfunction
 
   type header
 
-  type reprlist = sfunction * vfunction * header
+  type reprlist = {s: sfunction; v: vfunction; h: header}
 
   val create_of_list : int list -> reprlist
 
@@ -24,34 +23,33 @@ module type Reprlist = sig
 end
 
 module ReprlistInt = struct
-  type sfunction = (int -> int) * (int -> bool)
+  type sfunction = {s1: int -> int; s2: int -> bool}
 
   type vfunction = int -> int
 
   type header = int
 
-  type reprlist = sfunction * vfunction * header
+  type reprlist = {s: sfunction; v: vfunction; h: header}
 
   let composite g f idx = g (f idx)
 
-  let make_sfunction sfun1 sfun2: sfunction = (sfun1, sfun2)
-
   let rec vfunction_gen l i =
-     match l with
-     | [] -> (fun idx -> 0)
-     | h :: t -> (fun idx -> if idx == i then h else (vfunction_gen t (i + 1)) idx)
+    match l with
+    | [] -> fun idx -> 0
+    | h :: t ->
+        fun idx -> if idx == i then h else (vfunction_gen t (i + 1)) idx
 
-  let create_of_list (l: int list) =
-     let sfun1 idx = idx + 1 in
-     let len = List.length l in
-     let sfun2 idx = idx < len in
-     let vfun = vfunction_gen l 0 in
-     ((sfun1, sfun2), vfun, 0)
-     
+  let create_of_list (l : int list) =
+    let sfun1 idx = idx + 1 in
+    let len = List.length l in
+    let sfun2 idx = idx < len in
+    let vfun = vfunction_gen l 0 in
+    {s= {s1= sfun1; s2= sfun2}; v= vfun; h= 0}
 
   let rmatch (reprl : reprlist) : (int * reprlist) option =
-    match reprl with (sfun1, sfun2), vfun, idx -> (
-      if sfun2 idx then Some (vfun idx, ((sfun1, sfun2), vfun, (sfun1 idx))) else None )
+    if reprl.s.s2 reprl.h then
+      Some (reprl.v reprl.h, {reprl with h= reprl.s.s1 reprl.h})
+    else None
 
   let rec rfold_left f start reprl =
     match rmatch reprl with
@@ -60,27 +58,23 @@ module ReprlistInt = struct
         let start' = f start v in
         rfold_left f start' reprl'
 
-  let rmap f reprl =
-    match reprl with sfun, vfun, idx -> (sfun, composite f vfun, idx)
+  let rmap f reprl = {reprl with v= composite f reprl.v}
 
   let rec length_rec sfun idx len =
-    match sfun with sfun1, sfun2 -> (
-      if sfun2 idx then length_rec sfun (sfun1 idx) (len + 1) else len )
+    if sfun.s2 idx then length_rec sfun (sfun.s1 idx) (len + 1) else len
 
-  let length reprl =
-    match reprl with sfun, vfun, idx -> length_rec sfun idx 0
+  let length reprl = length_rec reprl.s reprl.h 0
 
   let reverse reprl =
     let len = length reprl in
-    match reprl with (sfun1, sfun2), vfun, idx ->
-      ( ( (fun i -> (len - 1) - sfun1 (len - 1 - i))
-        , fun i -> sfun2 (len - 1 - i) )
-      , vfun
-      , len - 1 )
+    { reprl with
+      s=
+        { s1= (fun i -> len - 1 - reprl.s.s1 (len - 1 - i))
+        ; s2= (fun i -> reprl.s.s2 (len - 1 - i)) }
+    ; h= len - 1 }
 
   let rec nth reprl n =
     match rmatch reprl with
     | None -> None
     | Some (v, reprl') -> if n = 0 then Some v else nth reprl' (n - 1)
 end
-
